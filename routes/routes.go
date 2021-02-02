043df24ad7344be879363126a805052bc0b7cb12
sakonphat <sakonphatb@gmail.com>
2021-02-03 02:18:21 +0700

    add cors for connecting with client

--- routes/routes.go
+++ routes/routes.go
@@ -7,22 +7,28 @@ import (
 )
 
 func SetupRouter() *gin.Engine {
+
 	r := gin.Default()
+
+	r.Use(middlewares.Cors())
+
 	api := "/api"
+
 	v1 := r.Group(api+"/v1")
 	{
 		v1.POST("/register", controllers.Register)
 		v1.POST("/login", controllers.Login)
 		v1.POST("/logout", controllers.Logout)
 
-		v1.POST("/task", middlewares.Authentication(), middlewares.ErrorHandler, controllers.CreateTask)
-		v1.GET("/task/:uuid", middlewares.Authentication(), middlewares.ErrorHandler, controllers.GetTask)
-		v1.GET("/tasks", middlewares.Authentication(), middlewares.ErrorHandler, controllers.GetAllTask)
-		v1.PUT("/edit", middlewares.Authentication(), middlewares.ErrorHandler, controllers.EditTask)
-		v1.PUT("/complete/:uuid", middlewares.Authentication(), middlewares.ErrorHandler, controllers.CompleteTask)
-		v1.PUT("/undo/:uuid", middlewares.Authentication(), middlewares.ErrorHandler, controllers.UndoTask)
-		v1.DELETE("/delete/:uuid", middlewares.Authentication(), middlewares.ErrorHandler, controllers.DeleteTask)
-		v1.DELETE("/delete-all", middlewares.Authentication(), middlewares.ErrorHandler, controllers.DeleteAllTask)
+		v1.POST("/task", middlewares.Authentication(), controllers.CreateTask)
+		v1.GET("/task/:uuid", middlewares.Authentication(), controllers.GetTask)
+		v1.GET("/tasks", middlewares.Authentication(), controllers.GetAllTask)
+		v1.PUT("/edit", middlewares.Authentication(), controllers.EditTask)
+		v1.PUT("/complete/:uuid", middlewares.Authentication(), controllers.CompleteTask)
+		v1.PUT("/undo/:uuid", middlewares.Authentication(), controllers.UndoTask)
+		v1.DELETE("/delete/:uuid", middlewares.Authentication(), controllers.DeleteTask)
+		v1.DELETE("/delete-all", middlewares.Authentication(), controllers.DeleteAllTask)
 	}
+
 	return r
 }
